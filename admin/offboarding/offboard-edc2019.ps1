5b780fc21701852a7ec31f738c416717fe5eed9e
Morten Pedersen <mortepe@statoil.com>
2019-09-27 09:23:11 +0200

    added script

---
+++ admin/offboarding/offboard-edc2019.ps1
@@ -0,0 +1,39 @@
+$context = Get-AzContext
+if ($null -eq $context )
+{
+    $context = Connect-AzAccount
+}
+
+$commonSubscriptionId = "160c90f1-6bbe-4276-91f3-f732cc0a45db"
+$commonSubscriptionName = "Omnia Application Workspace - Sandbox"
+
+Write-Host "Setting Subscription to $commonSubscriptionName ... " -NoNewline
+Set-AzContext -SubscriptionName $commonSubscriptionId | Out-Null
+Write-Host "Done" -ForegroundColor Green
+
+#Common Variables
+$edcAADGroup = "c5f931df-8725-4611-9594-378ec0a82c13"
+
+$commonRg = "EDC2019Common"
+$commonDls = "edc2019dls"
+
+
+$location = "northeurope"
+
+$adGroup = Get-AzADGroup -ObjectId $edcAADGroup
+
+$groupMembers = Get-AzADGroupMember -GroupObjectId $edcAADGroup
+
+foreach ($member in $groupMembers)
+{
+    if ($member.Type -eq "User")
+    {
+        $shortName = $member.UserPrincipalName.Split("@")[0].ToLower()
+        $resourceGroupName = "edc2019_$shortName"
+        Remove-AzResourceGroup -Name $resourceGroupName -WhatIf
+    }
+
+    Remove-AzADGroupMember -MemberObjectId $($member.Id) -GroupObjectId $edcAADGroup -WhatIf
+}
+
+Remove-AzResourceGroup -Name $commonRg -WhatIf
